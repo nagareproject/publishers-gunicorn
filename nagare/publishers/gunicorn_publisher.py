16e3cbabaee34ac959795a127f1728121d8a20cb
Alain Poirier <alain.poirier@net-ng.com>
2019-02-22 19:33:16 +0100

    Python 2

--- nagare/publishers/gunicorn_publisher.py
+++ nagare/publishers/gunicorn_publisher.py
@@ -10,7 +10,6 @@
 """The Gunicorn publisher"""
 
 import os
-import sys
 import multiprocessing
 from functools import partial
 
@@ -185,7 +184,7 @@ class Publisher(http_publisher.Publisher):
         super(Publisher, self)._serve(app_factory)
 
         if (reloader_service is not None) and self.has_multi_processes:
-            print("The reloader service can't be activated in multi-processes", file=sys.stderr)
+            print("The reloader service can't be activated in multi-processes")
             reloader_service = None
 
         config = {k: v for k, v in config.items() if (k not in http_publisher.Publisher.CONFIG_SPEC) and (v is not None)}
